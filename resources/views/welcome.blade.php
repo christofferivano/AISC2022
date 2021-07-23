8c116f6fc74349e0cdd02acc0b813c7f795808c1
Christoffer Ivano <christoffer.ivano@gmail.com>
2021-07-23 22:09:21 +0700

    yohoho

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!-- Penjelasan AISC -->
-    <div class="py-24 px-32">
-        <div class="rounded-md sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-50 h-140 flex justify-center items-center">
+    <div class="py-32 px-60">
+        <div class="rounded-md sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-40 h-120 flex justify-center items-center">
             <span class="font-bold text-4xl">
                 Penjelasan AISC
             </span>
