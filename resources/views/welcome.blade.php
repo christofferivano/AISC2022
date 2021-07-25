e6de9e13df78f0c18378e7fc982616016dd9e5c6
Christoffer Ivano <christoffer.ivano@gmail.com>
2021-07-26 01:13:25 +0700

    update landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,9 +16,9 @@
     <!-- Reminder -->
     <div class="relative pb-20 py-16">
         <div class="flex flex-col items-center">
-            <span class="text-footer text-4xl font-bold tracking-wide">
+            <h1 class="text-footer text-4xl font-bold tracking-wide">
                 Coming Very Soon!
-            </span>
+            </h1>
             <img src="images/reminder.png" alt="" class="w-3/5 pt-16 z-10">
         </div>
         <div class="absolute left-0 top-0 z-0 pl-20 pt-10">
@@ -27,72 +27,234 @@
     </div>
 
     <!-- List Event -->
-    <div class="container my-20 mx-auto px-52">
-        <div class="flex flex-wrap -mx-1">
-            <div class="my-4 px-2 w-1/3">
-                <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
-                    <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
-                        <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
-                        <span class="text-footer pt-2 pb-4">
-                            Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
-                        </span>
-                        <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
-                            <a href="" class="px-16 text-sm text-white">CEK</a>
-                        </button>
+    <div class="relative">
+        <div class="absolute bottom-0 right-0 z-0 -mb-32 pr-44">
+            <img src="images/bulet7.png" alt="" class="w-96">
+        </div>
+        <div class="container my-20 mx-auto px-52 z-10">
+            <div class="flex flex-wrap -mx-1">
+                <div class="my-4 px-2 w-1/3">
+                    <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
+                        <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
+                            <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
+                            <span class="text-footer pt-2 pb-4">
+                                Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
+                            </span>
+                            <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
+                                <a href="" class="px-16 text-sm text-white">CEK</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="my-4 px-2 w-1/3">
+                    <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
+                        <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
+                            <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
+                            <span class="text-footer pt-2 pb-4">
+                                Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
+                            </span>
+                            <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
+                                <a href="" class="px-16 text-sm text-white">CEK</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="my-4 px-2 w-1/3">
+                    <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
+                        <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
+                            <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
+                            <span class="text-footer pt-2 pb-4">
+                                Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
+                            </span>
+                            <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
+                                <a href="" class="px-16 text-sm text-white">CEK</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="my-4 px-2 w-1/3">
+                    <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
+                        <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
+                            <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
+                            <span class="text-footer pt-2 pb-4">
+                                Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
+                            </span>
+                            <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
+                                <a href="" class="px-16 text-sm text-white">CEK</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="my-4 px-2 w-1/3">
+                    <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
+                        <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
+                            <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
+                            <span class="text-footer pt-2 pb-4">
+                                Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
+                            </span>
+                            <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
+                                <a href="" class="px-16 text-sm text-white">CEK</a>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="my-4 px-2 w-1/3">
-                <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
-                    <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
-                        <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
-                        <span class="text-footer pt-2 pb-4">
-                            Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
-                        </span>
-                        <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
-                            <a href="" class="px-16 text-sm text-white">CEK</a>
-                        </button>
+        </div>
+    </div>
+
+    <!-- Timeline -->
+    <div class="py-10 px-20">
+        <div class="border border-red-200 rounded-lg shadow-lg w-full">
+            <div class="flex flex-col items-center py-10">
+                <h1 class="text-footer font-bold text-4xl">Timeline</h1>
+                <div class="flex flex-row items-center py-10">
+                    <button class="w-5 mr-20">
+                        <img src="images/left-timeline.png" alt="">
+                    </button>
+                    <div class="flex flex-row items-end">
+                        <div class="px-5">
+                            <div class="border h-52 w-72 bg-gradient-to-br from-purple-100 via-purple-400 to-purple-500 rounded-lg px-5 pt-28">
+                                <h1 class="text-gray-600 text-lg font-bold">Grand Theme</h1>
+                                <h1 class="text-gray-600 text-lg font-bold">Launching</h1>
+                                <p class="text-gray-600 text-md">Jul 30, 2021</p>
+                            </div>
+                        </div>
+                        <div class="px-5">
+                            <div class="border h-64 w-80 bg-gradient-to-tr from-red-600 via-red-500 to-yellow-400 rounded-lg px-5 pt-24">
+                                <h1 class="text-white text-2xl font-bold pt-2">Open</h1>
+                                <h1 class="text-white text-2xl font-bold">Recruitment</h1>
+                                <h1 class="text-white text-4xl font-bold">Volunteer</h1>
+                                <p class="text-white text-md pt-2">Aug 2, 2021</p>
+                            </div>
+                        </div>
+                        <div class="px-5">
+                            <div class="border h-52 w-72 bg-gradient-to-br from-gray-100 via-gray-300 to-gray-400 rounded-lg px-5 pt-32">
+                                <h1 class="text-gray-600 text-lg font-bold pt-3">Webinar 1</h1>
+                                <p class="text-gray-600 text-md">Sep 18, 2021</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="w-5 ml-20">
+                        <img src="images/right-timeline.png" alt="">
+                    </button>
+                </div>
+                <div class="flex flex-row items-center pl-52">
+                    <img src="images/tl-landing-1.png" alt="" class="w-1/4">
+                    <img src="images/tl-landing-2.png" alt="" class="w-1/3 ml-6">
+                    <img src="images/tl-landing-3.png" alt="" class="w-1/4 ml-2">
+                    <img src="images/tl-landing-4.png" alt="" class="w-10 ml-2">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Meet the Team -->
+    <div class="flex flex-col items-center py-14 bg-footer">
+        <h1 class="text-white font-bold text-4xl tracking-wide">
+            Meet The Team!
+        </h1>
+        <div class="px-16 py-10 flex items-center">
+            <button class="w-5">
+                <img src="images/left-team.png" alt="">
+            </button>
+            <div class="px-5">
+                <button class="member transition duration-200 px-8 py-1 rounded-full hover:bg-yellow-400 focus:bg-yellow-600 focus:shadow-2xl">
+                    <span class="text-xl font-semibold text-gray-600 focus:text-white">
+                        Web Developer
+                    </span>
+                </button>
+            </div>
+            <div class="px-5">
+                <button class="member transition duration-200 px-8 py-1 rounded-full hover:bg-yellow-400 focus:bg-yellow-600 focus:shadow-2xl">
+                    <span class="text-xl font-semibold text-gray-600 focus:text-white">
+                        Web Developer
+                    </span>
+                </button>
+            </div>
+            <div class="px-5">
+                <button class="member transition duration-200 px-8 py-1 rounded-full hover:bg-yellow-400 focus:bg-yellow-600 focus:shadow-2xl">
+                    <span class="text-xl font-semibold text-gray-600 focus:text-white">
+                        Web Developer
+                    </span>
+                </button>
+            </div>
+            <div class="px-5">
+                <button class="member transition duration-200 px-8 py-1 rounded-full hover:bg-yellow-400 focus:bg-yellow-600 focus:shadow-2xl">
+                    <span class="text-xl font-semibold text-gray-600 focus:text-white">
+                        Web Developer
+                    </span>
+                </button>
+            </div>
+            <button class="w-5">
+                <img src="images/right-team.png" alt="">
+            </button>
+        </div>
+        <div class="flex items-center py-10">
+            <div class="flex flex-col items-center px-6">
+                <div class="relative">
+                    <img src="images/border-team.png" alt="" class="w-64"> 
+                </div>
+                <div class="absolute py-10">
+                    <img src="images/vinsen.png" alt="" class="w-36 mr-3">
+                    <div class="flex flex-col items-center pt-4">
+                        <h2 class="text-white text-2xl font-semibold">
+                            Vinsen
+                        </h2>
+                        <h3 class="text-white text-md">
+                            Project Manager
+                        </h3>
                     </div>
                 </div>
             </div>
-            <div class="my-4 px-2 w-1/3">
-                <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
-                    <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
-                        <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
-                        <span class="text-footer pt-2 pb-4">
-                            Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
-                        </span>
-                        <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
-                            <a href="" class="px-16 text-sm text-white">CEK</a>
-                        </button>
+            <div class="flex flex-col items-center px-6">
+                <div class="relative">
+                    <img src="images/border-team.png" alt="" class="w-64"> 
+                </div>
+                <div class="absolute py-10">
+                    <img src="images/vinsen.png" alt="" class="w-36 mr-3">
+                    <div class="flex flex-col items-center pt-4">
+                        <h2 class="text-white text-2xl font-semibold">
+                            Vinsen
+                        </h2>
+                        <h3 class="text-white text-md">
+                            Project Manager
+                        </h3>
                     </div>
                 </div>
             </div>
-            <div class="my-4 px-2 w-1/3">
-                <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
-                    <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
-                        <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
-                        <span class="text-footer pt-2 pb-4">
-                            Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
-                        </span>
-                        <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
-                            <a href="" class="px-16 text-sm text-white">CEK</a>
-                        </button>
+            <div class="flex flex-col items-center px-6">
+                <div class="relative">
+                    <img src="images/border-team.png" alt="" class="w-64"> 
+                </div>
+                <div class="absolute py-10">
+                    <img src="images/vinsen.png" alt="" class="w-36 mr-3">
+                    <div class="flex flex-col items-center pt-4">
+                        <h2 class="text-white text-2xl font-semibold">
+                            Vinsen
+                        </h2>
+                        <h3 class="text-white text-md">
+                            Project Manager
+                        </h3>
                     </div>
                 </div>
             </div>
-            <div class="my-4 px-2 w-1/3">
-                <div class="card bg-gradient-to-r transition duration-500 ease-in-out hover:from-pink-500 hover:to-yellow-300 transform hover:-translate-y-2 hover:scale-105 overflow-hidden shadow-lg rounded-lg border border-gray-900 bg-cover h-landing bg-local relative h-52">
-                    <div class="card-event flex flex-col items-start py-3 px-4" id="card-event">
-                        <h2 class="underline text-footer text-xl font-semibold">Volunteer</h2>
-                        <span class="text-footer pt-2 pb-4">
-                            Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
-                        </span>
-                        <button class="py-1.5 rounded-full bg-gradient-to-r from-purple-400 to-red-500">
-                            <a href="" class="px-16 text-sm text-white">CEK</a>
-                        </button>
-                    </div>
-                </div>
+        </div>
+    </div>
+
+    <!-- Sponsor -->
+    <div class="flex flex-col items-center py-16">
+        <h1 class="text-footer text-4xl font-bold">
+            Sponsored By
+        </h1>
+        <div class="flex flex-row items-center pt-10">
+            <div class="px-16">
+                <img src="images/gojek.png" alt="" class="w-44">
+            </div>
+            <div class="px-16">
+                <img src="images/gojek.png" alt="" class="w-44">
+            </div>
+            <div class="px-16">
+                <img src="images/gojek.png" alt="" class="w-44">
             </div>
         </div>
     </div>
