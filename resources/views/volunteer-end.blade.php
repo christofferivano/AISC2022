1f3b9964c3b27c86b0846f53b0538f56eb67ef14
Christoffer Ivano <christoffer.ivano@gmail.com>
2021-07-28 22:32:55 +0700

    minor fix

--- resources/views/volunteer-end.blade.php
+++ resources/views/volunteer-end.blade.php
@@ -5,7 +5,7 @@
     <div class="pt-16 ml-80 pl-10">
         <img src="images/end-volunteer.png" alt="" class="w-5/7">
     </div>
-    <div class="py-10 text-montserrat flex flex-col items-center">
+    <div class="py-20 text-montserrat flex flex-col items-center pb-60">
         <h1 class="text-5xl font-bold">Thank You for Being The Volunteer of AISC 2022</h1>
         <h1 class="text-5xl font-bold pt-1 jusify-center">We Will Reach Out to You Soon!</h1>
     </div>
