27ec05b99ec3daf987a87d2dc1d8139f2297ab87
vincentyonathan <vincentyonathan24@gmail.com>
2021-08-21 00:01:00 +0700

    easy

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('aiscompetencyright');
+    return view('aiscomperegis');
 });
 
 // Route::get('competition', '');
