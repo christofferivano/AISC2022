2779dbeff1528628b8a4c97734aa4adfb86162e5
vincentyonathan <vincentyonathan24@gmail.com>
2021-10-28 19:04:26 +0700

    add coupon

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ use App\Http\Controllers\Competency\CompetencyController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::view('/dashboard', 'welcome')->name('dashboard');
+Route::view('/dashboard', 'couponposter')->name('dashboard');
 
 Route::get('/competency', [CompetencyController::class, 'index'])->name('competency');
 Route::get('/competency/registration', [CompetencyController::class, 'registration_1'])->name('competency-regis-1');
