59f7e9321cbbae1153d3d01bf57778925b6151b7
vincentyonathan <vincentyonathan24@gmail.com>
2022-01-11 19:20:14 +0700

    add page, -1

---
+++ resources/views/conferencedone.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.navbar-regis-volunteer')
+
+@section('content')
+
+    <div class="pt-28 pl-0 md:ml-28 md:pl-96" data-aos="fade-right" data-aos-duration="1500">
+        <img src="images/competency.png" alt="" class="h-100 md:h-140">
+    </div>
+    <div class="py-5 md:py-20 text-montserrat flex flex-col items-center pb-60" data-aos="fade-right" data-aos-duration="2000"> 
+        <h1 class="text-base md:text-4xl font-bold">Thank You for Keeping up with The Conferences</h1>
+        <h1 class="text-base md:text-4xl font-bold pt-1 jusify-center">We Hope You Enjoy the Show !</h1>
+    </div>
+
+    <!-- Footer -->
+    <footer class="bg-footer absolute w-full z-10">
+        <div class="h-full flex flex-col items-start">
+            <div class="px-10 md:px-12 lg:px-16 xl:px-20 py-8 md:py-10">
+                <h2 class="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white">
+                    Contact Us
+                </h2>
+                <div class="flex pt-2 md:pt-4 lg:pt-6 xl:pt-8 items-center">
+                    <img src="images/tiktok.png" alt="" class="w-6 md:w-8 lg:w-10 xl:w-12 mr-2 md:mr-4">
+                    <img src="images/instagram.png" alt="" class="w-6 md:w-8 lg:w-10 xl:w-12">
+                    <p class="pl-2 md:pl-5 text-white font-semibold text-sm md:text-base lg:text-lg xl:text-2xl">@aisc2022</p>
+                </div>
+                <div class="flex pl-8 md:pl-12 lg:pl-14 xl:pl-16 pt-2 md:pt-4 lg:pt-6 xl:pt-8 items-center">
+                    <img src="images/line-app.png" alt="" class="w-6 md:w-8 lg:w-10 xl:w-12">
+                    <p class="pl-2 xl:pl-5 text-white font-semibold text-sm md:text-base lg:text-lg xl:text-2xl">@961ckski</p>
+                </div>
+                <div class="flex pl-8 md:pl-12 lg:pl-14 xl:pl-16 pt-2 md:pt-4 lg:pt-6 xl:pt-8 items-center">
+                    <img src="images/linkedin.png" alt="" class="w-6 md:w-8 lg:w-10 xl:w-12">
+                    <p class="pl-2 xl:pl-5 text-white font-semibold text-sm md:text-base lg:text-lg xl:text-2xl">AIChE Indonesia Student Conference 2022</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+@endsection
